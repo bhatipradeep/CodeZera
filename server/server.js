baa3f8de806ef1e67b0dcc8b7954f566f41a7367
Jaynil Shah <jaynil.shah84@gmail.com>
2020-08-08 16:29:10 -0400

    public static express

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.listen(3000, () => {
     console.log(`started on port 3000`);
